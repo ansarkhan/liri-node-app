681a621eb7b928dc2b325af6b24ab8ab4d1687b4
ansarkhan <ansarkhan@me.com>
2019-02-18 21:02:13 -0500

    added all functionality

--- liri.js
+++ liri.js
@@ -3,6 +3,7 @@ var axios = require("axios");
 var keys = require("./keys.js");
 var moment = require("moment");
 var Spotify = require('node-spotify-api');
+var fs = require("fs");
 
 var arg1 = process.argv[2];
 var arg2 = process.argv[3];
@@ -10,90 +11,125 @@ var arg2 = process.argv[3];
 
 /*
 TO DOS
+- Give it a command line interface
 - add in date time module
-- make it all one function
+- default for Spotify API
 
 */
 
+// Concert API
+function concertThis(arg) {
+    var queryURL = `https://rest.bandsintown.com/artists/${arg}/events?app_id=codingbootcamp`;
 
+    axios.get(queryURL).then(
+        function(response) {
+            var events = response.data;
+            events.forEach(element => {
+                console.log(`Venue: ${element.venue.name}`);
+                console.log(`Venue location: ${element.venue.city}, ${element.venue.country}`);
+                console.log(`Date: ${element.datetime}`);
+                console.log('------------');
+            });
+        }
+    );
+
+};
+
+// Spotify API
+function spotifyThisSong(arg) {
+    var spotify = new Spotify({
+      id: keys.spotify.id,
+      secret: keys.spotify.secret,
+    });
+
+    // default to The Sign;
+     
+    spotify.search({ type: 'track', query: arg }, function(err, data) {
+      if (err) {
+        return console.log('Error occurred: ' + err);
+      }
+
+      var song = data.tracks.items[0];
+  
+      console.log(`Artist: ${song.artists[0].name}`);
+      console.log(`Song: ${song.name}`);
+      console.log(`Song Preview: ${song.preview_url}`);
+      console.log(`Album Name: ${song.album.name}`)
+    });
+};
+
+// OMDB API
+function movieThis(arg) {
+    var queryURL = `http://www.omdbapi.com/?t=${arg}&y=&plot=short&apikey=trilogy`;
+
+    axios.get(queryURL).then(
+        function(response) {
+            var movie = response.data
+            console.log(`Title: ${movie.Title}`);
+            console.log(`Release Year: ${movie.Year}`);
+            console.log(`IMDB Rating: ${movie.imdbRating}`);
+            console.log(`RT Rating: ${movie.Ratings[1].Value}`);
+            console.log(`Produce in: ${movie.Country}`);
+            console.log(`Language(s): ${movie.Language}`);
+            console.log(`Plot: ${movie.Plot}`);
+            console.log(`Actors: ${movie.Actors}`);
+        }
+    )
+};
+
+// reads text from file and calls function
+function doWhatItSays() {
+    fs.readFile("random.txt", "utf8", function(error, data) {
+
+        if (error) {
+            return console.log(error);
+        }
+    
+        var instructions = data.split(',');
+        arg1 = instructions[0];
+        arg2 = instructions[1];
+
+        switch(arg1) {
+
+            case 'concert-this':
+                concertThis(arg2);
+                break;
+        
+            case 'spotify-this-song':
+                spotifyThisSong(arg2);
+                break;
+        
+            case 'movie-this':
+                movieThis(arg2);
+                break;
+        
+            default:
+              console.log("choose another option");
+          }
+    
+      });
+}
+
+// main switch statement
 
 switch(arg1) {
 
     case 'concert-this':
-      console.log("concert api");
-      var queryURL = `https://rest.bandsintown.com/artists/${arg2}/events?app_id=codingbootcamp`;
-
-      axios.get(queryURL).then(
-          function(response) {
-              var events = response.data;
-              events.forEach(element => {
-                  console.log(`Venue: ${element.venue.name}`);
-                  console.log(`Venue location: ${element.venue.city}, ${element.venue.country}`);
-                  console.log(`Date: ${element.datetime}`);
-                  console.log('------------');
-              });
-          }
-      );
-
-      break;
+        concertThis(arg2);
+        break;
 
     case 'spotify-this-song':
-      console.log("spotify API");
-      var spotify = new Spotify({
-        id: keys.spotify.id,
-        secret: keys.spotify.secret,
-      });
-      // default to sign;
-       
-      spotify.search({ type: 'track', query: arg2 }, function(err, data) {
-        if (err) {
-          return console.log('Error occurred: ' + err);
-        }
-
-        var song = data.tracks.items[0];
-    
-        console.log(`Artist: ${song.artists[0].name}`);
-        console.log(`Song: ${song.name}`);
-        console.log(`Song Preview: ${song.preview_url}`);
-        console.log(`Album Name: ${song.album.name}`)
-      });
-
-      break;
+        spotifyThisSong(arg2);
+        break;
 
     case 'movie-this':
-        console.log("movie-api");
-        var queryURL = `http://www.omdbapi.com/?t=${arg2}&y=&plot=short&apikey=trilogy`;
-
-        axios.get(queryURL).then(
-            function(response) {
-                var movie = response.data
-                console.log(`Title: ${movie.Title}`);
-                console.log(`Release Year: ${movie.Year}`);
-                console.log(`IMDB Rating: ${movie.imdbRating}`);
-                console.log(`RT Rating: ${movie.Ratings[1].Value}`);
-                console.log(`Produce in: ${movie.Country}`);
-                console.log(`Language(s): ${movie.Language}`);
-                console.log(`Plot: ${movie.Plot}`);
-                console.log(`Actors: ${movie.Actors}`);
-            }
-        )
+        movieThis(arg2);
         break;
 
     case 'do-what-it-says':
-        console.log("do what it says api");
+        doWhatItSays();
         break;
+
     default:
       console.log("choose another option");
-  }
-
-
-//   
-// * Artist(s)
-
-// * The song's name
-
-// * A preview link of the song from Spotify
-
-// * The album that the song is from
-
-// * If no song is provided then your program will default to "The Sign" by Ace of Base.
+  };
